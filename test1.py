e12e56e8f94a284ea091c4addd7e5bfb0936861b
User <fdsf@>
2020-02-15 17:58:31 +0300

    №1

--- test1.py
+++ test1.py
@@ -1,6 +1,7 @@
 import os
 import sys
 import requests
+from PyQt5 import uic
 from PyQt5.QtGui import QPixmap
 from PyQt5.QtWidgets import QApplication, QWidget, QLabel
 
@@ -10,11 +11,20 @@ SCREEN_SIZE = [600, 450]
 class Example(QWidget):
     def __init__(self):
         super().__init__()
-        self.getImage()
-        self.initUI()
+        uic.loadUi('untitled.ui', self)
+        self.image = QLabel(self)
+        self.image.move(0, 0)
+        self.image.resize(500, 400)
+        self.pushButton.clicked.connect(self.showMap)
 
-    def getImage(self):
-        map_request = "http://static-maps.yandex.ru/1.x/?ll=37.530887,55.703118&spn=0.002,0.002&l=map"
+    def showMap(self):
+        print(1)
+        self.getImage(self.lineEdit.text() + ',' + self.lineEdit_2.text())
+        self.pixmap = QPixmap(self.map_file)
+        self.image.setPixmap(self.pixmap)
+
+    def getImage(self, coords):
+        map_request = f'http://static-maps.yandex.ru/1.x/?ll={coords}&spn=20,20&l=map&size=500,400'
         response = requests.get(map_request)
 
         if not response:
@@ -28,20 +38,15 @@ class Example(QWidget):
         with open(self.map_file, "wb") as file:
             file.write(response.content)
 
-    def initUI(self):
-        self.setGeometry(100, 100, *SCREEN_SIZE)
-        self.setWindowTitle('Отображение карты')
-
-        ## Изображение
-        self.pixmap = QPixmap(self.map_file)
-        self.image = QLabel(self)
-        self.image.move(0, 0)
-        self.image.resize(600, 450)
-        self.image.setPixmap(self.pixmap)
-
     def closeEvent(self, event):
         """При закрытии формы подчищаем за собой"""
         os.remove(self.map_file)
+
+    def keyPressEvent(self, e):
+        print(e.key())
+        key = e.key()
+        if key == 16777234:
+            self.getImage()
 
 
 if __name__ == '__main__':
